4d98c2ad39368e0c15045cdd7789a81baf0ac5f7
uratamanabu <mabu_ip@msn.com>
2018-05-02 08:22:34 +0900

    サブプロジェクト追加

---
+++ micro-interfaces/src/main/java/jp/team/study/book/manage/interfaces/definitions/requests/Sample.java
@@ -0,0 +1,4 @@
+package jp.team.study.book.manage.interfaces.definitions.requests;
+@Deprecated
+public class Sample {
+}
---
+++ micro-interfaces/src/main/java/jp/team/study/book/manage/interfaces/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * BookManagerAPI
+ * 社内書籍管理システムのIF層です。
+ */
+package jp.team.study.book.manage.interfaces;
