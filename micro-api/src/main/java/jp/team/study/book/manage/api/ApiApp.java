5db1076d357c330f2af659bc00b958a2f74ed6f6
uratamanabu <mabu_ip@msn.com>
2018-05-02 08:22:54 +0900

    サブプロジェクト追加

---
+++ micro-api/src/main/java/jp/team/study/book/manage/api/ApiApp.java
@@ -0,0 +1,35 @@
+package jp.team.study.book.manage.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * SpringBoot起動クラスです。
+ *
+ * @author uratamanabu
+ * @version 1.0
+ * @since 1.0
+ */
+@SpringBootApplication
+@EntityScan(basePackageClasses = {ApiApp.class})
+public class ApiApp extends SpringBootServletInitializer {
+
+    /**
+     * SpringBoot起動methodです。
+     *
+     * @param args 起動引数
+     * @author uratamanabu
+     * @since 1.0
+     */
+    public static void main(final String[] args) {
+        new SpringApplicationBuilder(ApiApp.class).run(args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
+        return builder.sources(ApiApp.class);
+    }
+
+}
---
+++ micro-api/src/main/java/jp/team/study/book/manage/api/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * BookManagerAPI
+ * 社内書籍管理システムのAPI層です。
+ */
+package jp.team.study.book.manage.api;
